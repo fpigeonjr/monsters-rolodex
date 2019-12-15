0d9a67ef945f53b710e37bb211f5e0a581d5d7aa
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-12-14 23:34:03 -0600

    initial commit

---
+++ src/components/card/card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./card-styles.css";
+
+export const Card = ({ monster }) => (
+  <div className="card-container">
+    <img
+      src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+      alt="monster"
+    />
+    <h2>{monster.name}</h2>
+    <p>{monster.email}</p>
+  </div>
+);
