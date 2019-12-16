24565c5f8915f2377a3f42af7d560cb6bbcd7fa2
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-12-15 22:36:43 -0600

    makes seachbox its own comp and adds styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import { CardList } from './components/card-list/card-list'
+import { SearchBox } from './components/search-box/search-box'
 
 class App extends Component {
   constructor() {
@@ -23,11 +24,11 @@ class App extends Component {
     return (
       <>
         <div className="App">
-          <input
-            type="search"
+          <SearchBox
             placeholder="search monsters"
-            onChange={e => this.setState({ searchField: e.target.value })}
+            handleChange={e => this.setState({ searchField: e.target.value })}
           />
+
           <CardList monsters={filteredMonsters} />
         </div>
       </>
