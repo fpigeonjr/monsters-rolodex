88b948f524edfd610ff3f91a9de6ecf1174f5dc1
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-12-16 21:49:36 -0600

    Updates

---
+++ precache-manifest.c005e3743ac4998f9920f57450ef32ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017ed3c14656983a10fa038f897bb53b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc70df0c10f4db7c60ac",
+    "url": "/monsters-rolodex/static/css/main.88a7497a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc70df0c10f4db7c60ac",
+    "url": "/monsters-rolodex/static/js/main.4a945413.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
