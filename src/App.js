4c16d5a574c62b0bd5dc61ca9c37877a7637e5f4
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-12-16 21:39:41 -0600

    adds google fonts and app bg

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     return (
       <>
         <div className="App">
+          <h1>Monsters Rolodex</h1>
           <SearchBox
             placeholder="search monsters"
             handleChange={e => this.setState({ searchField: e.target.value })}
